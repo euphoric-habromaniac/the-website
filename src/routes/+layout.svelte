bac7947b6fcdcb04341e155e37c85b188a91f12f
euphoric-habromaniac <rulerofantarctica1024@gmail.com>
2025-06-15 17:29:06 +0530

    fixed html errors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,18 +15,20 @@
     class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
     <div
-        class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
-            (y > 0
-                ? " opacity-full pointer-events-auto"
-                : " pointer-events-none opacity-0")}
-    >
-        <button
-            on:click={goTop}
-            class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
-        >
-            <i class="fa-solid fa-arrow-up" />
-        </button>
-    </div>
+	class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+		(y > 0
+			? " opacity-full pointer-events-auto"
+			: " pointer-events-none opacity-0")}
+>
+	<button
+		on:click={goTop}
+		aria-label="Scroll to top"
+		class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+	>
+		<i class="fa-solid fa-arrow-up"></i>
+	</button>
+</div>
+
     <Header {y} {innerHeight}/>
     <slot />
     <Footer />
